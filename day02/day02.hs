2eb5ba203e15eff07debe2433c10262cfe4e6d42
Artur Alves <arturacb@gmail.com>
2024-12-06 15:06:22 -0300

    day02 complete

--- day02/day02.hs
+++ day02/day02.hs
@@ -1,23 +1,37 @@
 import Text.Printf
+import Data.List 
 
 parseInput :: [Char] -> [[Int]]
 parseInput = map (map read . words) . lines
 
-listIsSafe :: [Int] -> Int
+listIsSafe :: [Int] -> Bool 
 listIsSafe x 
-    | moreThan3 l = 0
-    | notAllDecrIncr x = 0
-    | otherwise = 1
+    | moreThan3 l = False 
+    | notAllDecrIncr x = False 
+    | otherwise = True 
   where
     l = zipWith (-) (tail x) x
     moreThan3 x = any (> 3) x || any (< -3) x
     notAllDecrIncr x = not $ (all (uncurry (<)) (zip x (tail x)))
                           || (all (uncurry (>)) (zip x (tail x)))
 
+removeOne :: [Int] -> [[Int]]
+removeOne [] = []
+removeOne (x:xs) = xs : map (x:) (removeOne xs)
+
+canBeSafe :: [Int] -> Bool
+canBeSafe xs
+    | null xs = False
+    | otherwise = any listIsSafe (removeOne xs)
+
 solvePt1 :: [[Int]] -> Int
-solvePt1 xs = sum (map listIsSafe xs)
+solvePt1 xs = length $ filter listIsSafe xs
+
+solvePt2 :: [[Int]] -> Int
+solvePt2 xs = length $ filter canBeSafe (filter (not . listIsSafe) xs)
 
 main :: IO()
 main = do
   input <- parseInput <$> readFile "input.in"
   printf "Part 1: %d\n" (solvePt1 input)
+  printf "Part 2: %d\n" (solvePt1 input + solvePt2 input)
