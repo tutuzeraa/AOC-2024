978b00fc489cac912a5c42a6c749c76308f1a99b
Artur Alves <arturacb@gmail.com>
2024-12-18 17:23:34 -0300

    day03 complete

--- day03/day03.hs
+++ day03/day03.hs
@@ -21,8 +21,23 @@ solvePt1 s@(_:xs) res
             (n2, ')':xs2) <- tryParseNumber xs1
             return (n1 * n2) 
 
+solvePt2 :: String -> Bool -> Int -> Int
+solvePt2 [] bool res = res
+solvePt2 s@(_:xs) bool res 
+    | "do()" `isPrefixOf` s = solvePt2 xs True res
+    | "don't()" `isPrefixOf` s = solvePt2 xs False res
+    | "mul(" `isPrefixOf` s = if bool then solvePt2 xs bool res + tryMul else solvePt2 xs bool res
+    | otherwise = solvePt2 xs bool res
+    where
+        tryMul =
+            fromMaybe 0 $ do
+            (n1, ',':xs1) <- tryParseNumber (drop 4 s)
+            (n2, ')':xs2) <- tryParseNumber xs1
+            return (n1 * n2) 
+
 
 main :: IO()
 main = do
     input <- readFile "input.in"
     printf "Part 1: %d\n" (solvePt1 input 0)
+    printf "Part 2: %d\n" (solvePt2 input True 0)
