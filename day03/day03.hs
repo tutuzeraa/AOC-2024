b421dddbef6247134408ad4b914756e1de25a82c
Artur Alves <arturacb@gmail.com>
2024-12-18 16:01:42 -0300

    day03 pt1

--- day03/day03.hs
+++ day03/day03.hs
@@ -1,10 +1,28 @@
 import Text.Printf
-import Data.List.Split -- You must install :/
+import Data.List
+import Data.Maybe
+import Data.Char
 
-solvePt1 :: String -> Int
-solvePt1 = undefined
+tryParseNumber :: String -> Maybe (Int, String)
+tryParseNumber s =
+    case span isDigit s of
+        ("", rest) -> Nothing
+        (n, rest) -> Just (read n, rest)
+
+solvePt1 :: String -> Int -> Int
+solvePt1 [] res = res
+solvePt1 s@(_:xs) res 
+    | "mul(" `isPrefixOf` s = solvePt1 xs res + tryMul
+    | otherwise = solvePt1 xs res
+    where
+        tryMul =
+            fromMaybe 0 $ do
+            (n1, ',':xs1) <- tryParseNumber (drop 4 s)
+            (n2, ')':xs2) <- tryParseNumber xs1
+            return (n1 * n2) 
+
 
 main :: IO()
 main = do
     input <- readFile "input.in"
-    printf "Part 1: %d\n" (solvePt1 input)
+    printf "Part 1: %d\n" (solvePt1 input 0)
